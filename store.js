cd5a20d911f5924867a35e823383f0b73c840636
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-03-31 21:04:29 -0700

    quiz app wireframe init

---
+++ store.js
@@ -0,0 +1,109 @@
+const STORE = [
+  // 1
+  {
+    question:
+      "Sarah wants to drive from point A to point B using the fastest route. Which route would be the fastest using the formula below?",
+    answers: [
+      {
+        label: "Route1 is 360miles & constant speed is 75mph",
+        answer: "Fastest route is 288 minutes",
+        correct: false
+      },
+      {
+        label: "Route2 is 470miles & constant speed is 100mph",
+        answer: "Fastest route is 282 minutes",
+        correct: true
+      },
+      {
+        label: "Route3 is 560miles & constant speed is 112mph",
+        answer: "Fastest route is 300 minutes",
+        correct: false
+      },
+      {
+        label: "Route4 is 323miles & constant speed is 68mph",
+        answer: "Fastest route is 285 minutes",
+        correct: false
+      }
+    ]
+  },
+  {
+    question:
+      "A van moves with a constant speed of 84 miles per hour. Use the formula above to calculate how long will it take to travel a distance of 126 miles?",
+    answers: [
+      {
+        label: "3 hours",
+        answer: "3 hours",
+        correct: false
+      },
+      {
+        label: "1.5 hours",
+        answer: "1.5 hours",
+        correct: true
+      },
+      {
+        label: "1 hours",
+        answer: "1 hours",
+        correct: false
+      },
+      {
+        label: "1.65 hours",
+        answer: "1.65 hours",
+        correct: false
+      }
+    ]
+  },
+
+  {
+    question:
+      "An airplane flies with a constant speed of 580mph. How long will it take to travel a distance of 1160 miles?",
+    answers: [
+      {
+        label: "3 hours",
+        answer: "3 hours",
+        correct: false
+      },
+      {
+        label: "1.5 hours",
+        answer: "1.5 hours",
+        correct: false
+      },
+      {
+        label: "2 hours",
+        answer: "2 hours",
+        correct: true
+      },
+      {
+        label: "1.65 hours",
+        answer: "1.65 hours",
+        correct: false
+      }
+    ]
+  },
+
+  {
+    question:
+      "A motorcyle drives with a constant speed of 52 miles per hour. How long will it take to travel a distance of 169 miles?",
+    answers: [
+      {
+        label: "3 hours",
+        answer: "3 hours",
+        correct: false
+      },
+      {
+        label: "1.5 hours",
+        answer: "1.5 hours",
+        correct: false
+      },
+      {
+        label: "2 hours",
+        answer: "2 hours",
+        correct: false
+      },
+      {
+        label: "3.25 hours",
+        answer: "3.25 hours",
+        correct: true
+      }
+    ]
+  }
+];
